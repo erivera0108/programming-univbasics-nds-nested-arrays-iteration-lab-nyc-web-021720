e31c738936b2f27bc4bd7676720a6d09af72c8e5
Edgar Rivera <edgar.a.rivera08@gmail.com>
2020-02-04 21:18:42 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -25,7 +25,7 @@ result = []
     if src[row_index][0] > src[row_index][1]
       result << src[row_index][0]
     else
-      result << [row_index][1]
+      result << src[row_index][1]
     end
     row_index += 1
   end
